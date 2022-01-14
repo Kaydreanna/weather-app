799d0c7100dbe66fb3f4c3023956de41b8ae6959
Kaydreanna <94328465+Kaydreanna@users.noreply.github.com>
2022-01-14 08:38:51 -0700

    Restructured Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import "./styles.css";
+import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/kaydreanna-castaneda-345637211"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kaydreanna Castaneda
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/Kaydreanna/weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://naughty-jones-3cb7a1.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
